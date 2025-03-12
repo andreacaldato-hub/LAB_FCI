de8f522e9d68c43d41a8be4f6ebac6fc9bb358f4
andreacaldato-hub <andreacaldato6@gmail.com>
2025-03-12 11:09:58 +0100

    for

--- L1/richiesta_http.py
+++ L1/richiesta_http.py
@@ -2,5 +2,7 @@
 import requests
 
 # Effetto richista GET
-r = requests.get("http://google.com")
-print(type(r))
+for i in range(10):
+    r = requests.get("http://google.com")
+    print(f"Tempo di risposta: {r.elapsed.microseconds / 1000} ms")
+# print(r.content[:500])  # Prints only the first 500 bytesrint(r.content)
