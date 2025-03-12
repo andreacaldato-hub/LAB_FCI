0f042b6147800deb4aa3d576df41bc7bb8ed83c1
andreacaldato-hub <andreacaldato6@gmail.com>
2025-03-12 11:32:22 +0100

    ciao

--- L1/stistiche.py
+++ L1/stistiche.py
@@ -1,11 +1,16 @@
 import requests
 
 # Effetto richista GEjjjT
-n_richeste = 20
-sito = "http://google.com"
-risorse = []
+n_richeste = 10
+sito = "http://www.tiktok.com"
+Lista_tempo = []
 for i in range(n_richeste):
     r = requests.get(sito)
     tempo = r.elapsed.microseconds / 1000
-    print(f"Tempo di risposta: {tempo} ms")
+    Lista_tempo.append(tempo)
 # print(r.content[:500])  # Prints only the first 500 bytesrint(r.content)
+print(f"Lista_tempo: {Lista_tempo}")
+min = min(Lista_tempo)
+max = max(Lista_tempo)
+medio = sum(Lista_tempo) / len(Lista_tempo)
+print(f"Min: {min} ms \nMax: {max} ms\nMedio: {medio} ms")
