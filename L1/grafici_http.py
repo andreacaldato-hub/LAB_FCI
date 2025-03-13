599ace643fe531a415c867e8616716cb3545e3ca
andreacaldato-hub <andreacaldato6@gmail.com>
2025-03-13 18:46:49 +0100

    ciao

--- L1/grafici_http.py
+++ L1/grafici_http.py
@@ -3,15 +3,15 @@ from matplotlib import pyplot as plt
 
 n_richeste = 20
 sito = "http://www.polimi.it"
-Lista_tempo = []
+tempi = []
 for i in range(n_richeste):
     r = requests.get(sito)
     tempo = r.elapsed.microseconds / 1000
-    Lista_tempo.append(tempo)
+    tempi.append(tempo)
 # print(r.content[:500])  # Prints only the first 500 bytesrint(r.content)
-min = min(Lista_tempo)
-max = max(Lista_tempo)
-medio = sum(Lista_tempo) / len(Lista_tempo)
+min = min(tempi)
+max = max(tempi)
+medio = sum(tempi) / len(tempi)
 print(f"Min: {min} ms \nMax: {max} ms\nMedio: {medio} ms")
 
 plt.figure()
@@ -22,8 +22,8 @@ plt.axhline(min, color="#C45A5A", label="min")
 plt.axhline(max, color="#E3A01F", label="max")
 plt.axhline(medio, color="#219D8C", label="medio")
 plt.grid()
-plt.plot(Lista_tempo, label=sito)
-plt.plot(Lista_tempo, label=medio)
+plt.plot(tempi, label=sito)
+plt.plot(tempi, label=medio)
 plt.ylim([min, max])
 plt.show()
 plt.legend(loc=1, fontsize=25)
